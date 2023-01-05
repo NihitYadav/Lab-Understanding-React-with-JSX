f8b3b05b0b3a20960c4cd6d63cef7a0b63510616
NihitYadav <112609624+NihitYadav@users.noreply.github.com>
2023-01-05 16:38:41 +0530

    Add files via upload

---
+++ lab 2 on react-jsx-boilerplate-main/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import elephant from "./images/elephant.jpeg";
+
+const imageData = ()=>{
+  let data = [
+    {
+      id:1,
+      img:elephant
+    },
+    {
+      id:2,
+      img:elephant
+    },
+    {
+      id:3,
+      img:elephant
+    },
+    {
+      id:4,
+      img:elephant
+    }
+  ]
+  return data;
+}
+
+function App() {
+  // code here
+  let imgdata = imageData();
+  console.log(imgdata);
+  
+  return (
+    <div className="App">
+      <h1>Kalvium Gallery</h1>
+
+      <div className="image">
+        <img src={imgdata[0].img} alt="ele" />
+        <img src={imgdata[1].img} alt="ele" />
+        <img src={imgdata[2].img} alt="ele" />
+        <img src={imgdata[3].img} alt="ele" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
